70adffcb74dbe121216fd031da193afbb1cfb2a6
Guilherme Goulart Magela <guilhermegoulartmagela@gmail.com>
2023-11-21 09:08:55 -0300

    ending get routes, and creating the put route.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,47 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const livros = [
-   {
-      id: 1,
-      nome: "O Senhor dos Anéis",
-   },
-   {
-      id: 2,
-      nome: "Harry Potter",
-   },
-]
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    titulo: "Harry Potter",
+  },
+];
 
-app.get("/", (req,res) => {
-   res.status(200).send("Curso de Node.js");
+function buscaLivro(id) {
+  return livros.findIndex((livro) => {
+    return livro.id === Number(id);
+  });
+}
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req,res) => {
-   res.status(200).json(livros);
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.status(200).json(livros);
 });
 
 export default app;
