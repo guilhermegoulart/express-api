7531dd3579769c3e2d2008fbb3d11d0b7a84ea85
Guilherme Goulart Magela <guilhermegoulartmagela@gmail.com>
2023-11-25 09:14:08 -0300

    creating a connection with data base, and import a env to improve security

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectInDataBase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
 
 const connection = await connectInDataBase();
 
@@ -14,29 +15,13 @@ connection.once("open", () => {
 const app = express();
 app.use(express.json());
 
-/* const livros = [
-  {
-    id: 1,
-    titulo: "O Senhor dos Anéis",
-  },
-  {
-    id: 2,
-    titulo: "Harry Potter",
-  },
-]; */
-
-function buscaLivro(id) {
-  return livros.findIndex((livro) => {
-    return livro.id === Number(id);
-  });
-}
-
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
 
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({})
+  res.status(200).json(listaLivros);
 });
 
 app.get("/livros/:id", (req, res) => {
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function connectInDataBase() {
-  mongoose.connect(
-    "mongodb+srv://guilhermegoulartm:AristeuGoulart@cluster0.ws6nnbb.mongodb.net/livraria?retryWrites=true&w=majority"
-  );
-
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
   return mongoose.connection;
 }
 
